3df4f4f66799e138154f21f117eff7c4f3f8adec
Jette7298 <jette.roggow@gmail.com>
2026-01-12 16:38:39 +0100

    energy over time added

--- Scripts/Visualization_xyz.R
+++ Scripts/Visualization_xyz.R
@@ -23,3 +23,17 @@ multi_users |>
   labs(title = "Acceleration distributions (x, y, z) across 10 random subjects",
        x = "Acceleration", y = "Count")
 
+#energy over time
+acc_all_data <- acc_all_data |>
+  mutate(magnitude = sqrt(x^2 + y^2 + z^2))
+
+multi_users <- acc_all_data |>
+  semi_join(random_subjects, by = "subject_path") |>
+  mutate(magnitude = sqrt(x^2 + y^2 + z^2))
+
+multi_users |>
+  ggplot(aes(x = timestamp_ms, y = magnitude)) +
+  geom_line(alpha = 0.6) +
+  facet_wrap(~ subject_path, scales = "free_y") +
+  labs(title = "Movement energy over time",
+       x = "Time (ms)", y = "Acceleration magnitude")
